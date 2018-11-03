3a13decbe2ed3c4ad0436a4783c3ceb580ae14d0
danielhanbitlee <danielhanbitlee@gmail.com>
2018-11-02 17:13:40 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Analysis of the Titanic dataset
+This proejct aims at analyzing Kaggle's Titanic dataset and build a predictive model for the Titanic data science challenge.
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.6
+from distutils.core import setup
+
+setup(name='titanic',
+      version='1.0',
+      description="Analyze Kaggle's Titanic dataset and build a predictive model for the Titanic data science challenge",
+      author='Daniel Lee',
+      author_email='danielhanbitlee@gmail.com',
+      url='',
+      packages=['titanic'],
+     )
