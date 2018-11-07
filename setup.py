b55f5e5ee07ee0bda3dd7d5f96461ca104a66ebf
danielhanbitlee <danielhanbitlee@gmail.com>
2018-11-07 11:05:42 -0800

    Add data pipeline and command line tool to launch it

--- setup.py
+++ setup.py
@@ -1,16 +1,44 @@
-#!/usr/bin/env python3.6
 from distutils.core import setup
 
-setup(name='titanic',
-      version='1.0',
-      description="Analyze Kaggle's Titanic dataset and build a predictive model for the Titanic data science challenge",
-      author='Daniel Lee',
-      author_email='danielhanbitlee@gmail.com',
-      url='',
-      packages=['titanic'],
-      install_requires=[
-	'pypandoc>=1.4'
-      ],
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
-     )
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+setup(
+    name='titanic',
+    version='0.1',
+    description='Analysis of the Titanic dataset',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    url='https://github.com/danielhanbitlee/titanic_datascience',
+    author='Daniel Lee',  
+    author_email='danielhanbitlee@gmail.com',  
+    license='MIT',
+    packages=['titanic'],
+    install_requires=[
+        'pypandoc>=1.4',
+        'watermark>=1.5.0',
+        'pandas>=0.20.3',
+        'scikit-learn>=0.19.0',
+        'scipy>=0.19.1',
+        'matplotlib>=2.1.0',
+        'pytest>=3.2.3',
+        'pytest-runner>=2.12.1',
+        'click>=6.7'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points='''
+        [console_scripts]
+        titanic_analysis=titanic.command_line:titanic_analysis
+    '''
+)
